84030efa5562e2e1abcc731c2ecb2e8312f7e81f
jackprator <jackprator@gmail.com>
2021-12-13 22:27:43 -0500

    submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,54 @@
 const miles = document.querySelector("#question1");
 const vehicle = document.querySelector("#question2");
 const meat = document.querySelector("#question3");
-const result =  document.querySelector("#results");
 
 document.body.addEventListener('input', getResult);
 
 function getResult() {
   let vMiles = parseFloat(miles.value);
   let vVehicle = parseFloat(vehicle.value);
-  let vMeat = parseFloat(meat.value);
+  let vMeat = parseFloat(meat.value) * 100;
+
 
   let vResult = parseFloat(vMiles) + parseFloat(vVehicle) + parseFloat(vMeat);
-  result.value = vResult;
+
+  var x = document.getElementById("goodDiv");
+  var y = document.getElementById("badDiv");
+  var z = document.getElementById("neutralDiv");
+  z.style.display = "none";
+  y.style.display = "none";
+  x.style.display = "none";
+  if (vResult <= 500) {
+    x.style.display = "block";
+  } else if (vResult >= 751){
+    y.style.display = "block";
+  } else if (vResult >= 501 && vResult <= 750){
+    z.style.display = "block";
+  }
+}
+
+// hide/show navbar divs
+function clickMiles() {
+  var x = document.getElementById("milesDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function clickVehicle() {
+  var x = document.getElementById("vehicleDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function clickMeat() {
+  var x = document.getElementById("meatDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
